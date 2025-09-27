704e11e466e8b2c90177af54defadd07d3b55e5a
✞♕𝕯𝖆𝖓𝖎𝖑𝖔𝖛𝖎𝖖𝖚𝖎𝖘♕✞ <145468977+Danilo-RC@users.noreply.github.com>
2025-09-26 21:17:13 -0300

    feat: proteção na rota de perfil

--- aula/routes/web.php
+++ aula/routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+// Contato público
 Route::get('/Contato', function () {
     return view('Contato');
 })->name('Contato');
@@ -44,18 +45,21 @@ Route::middleware(['auth', 'nivel:0'])->group(function () {
 
 // Rotas protegidas por autenticação e nível de acesso (usuário comum)
 Route::middleware(['auth', 'nivel:1'])->group(function () {
+    // Página inicial protegida para usuário comum
     Route::get('/home', function () {
         return view('welcome');
     })->name('home.protected');
+
+    // Perfil do usuário
+    Route::get('/Perfil', function () {
+        return view('Perfil');
+    })->name('perfil');
+
+    // Atualização da foto de perfil
+    Route::put('/perfil/foto', [UsuarioController::class, 'fotoPerfil'])
+        ->name('perfil.foto');
 });
 
 // Contato
 Route::get('/Contato', 'App\Http\Controllers\ContatoController@exibirContato');
 Route::post('/Contato/inserir', 'App\Http\Controllers\ContatoController@store');
-
-// Perfil do usuário
-Route::get('/Perfil', function () {
-    return view('Perfil');
-});
-
-Route::middleware('auth')->put('/perfil/foto', [UsuarioController::class, 'fotoPerfil'])->name('perfil.foto');
